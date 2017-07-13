f9cdf7d57eb2b147b464626d59da66f0254c718d
Hugo Gutiérrez <hugogutiok@outlook.com>
2017-07-13 04:22:50 -0300

    Added some buttons

--- Main/CG-Project/Form1.Designer.cs
+++ Main/CG-Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CG_Project
 {
-    partial class Form1
+    partial class MainWindowsPCG
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,20 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(137, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CLIENTE";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // MainWindowsPCG
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1008, 561);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Name = "MainWindowsPCG";
+            this.Text = "Project CG";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Main/CG-Project/Program.cs
+++ Main/CG-Project/Program.cs
@@ -16,7 +16,7 @@ namespace CG_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindowsPCG());
         }
     }
 }
