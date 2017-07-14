b6715747ba9c1d5edbc99956ec8b3c2e89f40637
Hugo Gutiérrez <hugogutiok@outlook.com>
2017-07-14 04:20:52 -0300

    I added some buttoms and change the interface looks

--- Main/CG-Project/Form1.Designer.cs
+++ Main/CG-Project/Form1.Designer.cs
@@ -28,40 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindowsPCG));
+            this.botonAgregarCliente = new System.Windows.Forms.Button();
+            this.botonAgregarProducto = new System.Windows.Forms.Button();
+            this.botonAgregarServicioVendido = new System.Windows.Forms.Button();
+            this.botonVisualizarClientes = new System.Windows.Forms.Button();
+            this.botonVisualizarProductos = new System.Windows.Forms.Button();
+            this.botonVisualizarEstadisticas = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // botonAgregarCliente
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(137, 165);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "CLIENTE";
-            this.button1.UseVisualStyleBackColor = false;
+            this.botonAgregarCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.botonAgregarCliente.FlatAppearance.BorderSize = 0;
+            this.botonAgregarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonAgregarCliente.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAgregarCliente.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonAgregarCliente.Location = new System.Drawing.Point(175, 190);
+            this.botonAgregarCliente.Name = "botonAgregarCliente";
+            this.botonAgregarCliente.Size = new System.Drawing.Size(236, 53);
+            this.botonAgregarCliente.TabIndex = 0;
+            this.botonAgregarCliente.Text = "CLIENTE";
+            this.botonAgregarCliente.UseVisualStyleBackColor = false;
+            // 
+            // botonAgregarProducto
+            // 
+            this.botonAgregarProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.botonAgregarProducto.FlatAppearance.BorderSize = 0;
+            this.botonAgregarProducto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonAgregarProducto.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAgregarProducto.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonAgregarProducto.Location = new System.Drawing.Point(175, 258);
+            this.botonAgregarProducto.Name = "botonAgregarProducto";
+            this.botonAgregarProducto.Size = new System.Drawing.Size(236, 53);
+            this.botonAgregarProducto.TabIndex = 1;
+            this.botonAgregarProducto.Text = "PRODUCTO";
+            this.botonAgregarProducto.UseVisualStyleBackColor = false;
+            // 
+            // botonAgregarServicioVendido
+            // 
+            this.botonAgregarServicioVendido.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.botonAgregarServicioVendido.FlatAppearance.BorderSize = 0;
+            this.botonAgregarServicioVendido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonAgregarServicioVendido.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAgregarServicioVendido.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonAgregarServicioVendido.Location = new System.Drawing.Point(175, 328);
+            this.botonAgregarServicioVendido.Name = "botonAgregarServicioVendido";
+            this.botonAgregarServicioVendido.Size = new System.Drawing.Size(236, 53);
+            this.botonAgregarServicioVendido.TabIndex = 2;
+            this.botonAgregarServicioVendido.Text = "SERVICIO VENDIDO";
+            this.botonAgregarServicioVendido.UseVisualStyleBackColor = false;
+            // 
+            // botonVisualizarClientes
+            // 
+            this.botonVisualizarClientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.botonVisualizarClientes.FlatAppearance.BorderSize = 0;
+            this.botonVisualizarClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonVisualizarClientes.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonVisualizarClientes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonVisualizarClientes.Location = new System.Drawing.Point(599, 190);
+            this.botonVisualizarClientes.Name = "botonVisualizarClientes";
+            this.botonVisualizarClientes.Size = new System.Drawing.Size(236, 53);
+            this.botonVisualizarClientes.TabIndex = 3;
+            this.botonVisualizarClientes.Text = "CLIENTE/S";
+            this.botonVisualizarClientes.UseVisualStyleBackColor = false;
+            // 
+            // botonVisualizarProductos
+            // 
+            this.botonVisualizarProductos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.botonVisualizarProductos.FlatAppearance.BorderSize = 0;
+            this.botonVisualizarProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonVisualizarProductos.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonVisualizarProductos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonVisualizarProductos.Location = new System.Drawing.Point(599, 258);
+            this.botonVisualizarProductos.Name = "botonVisualizarProductos";
+            this.botonVisualizarProductos.Size = new System.Drawing.Size(236, 53);
+            this.botonVisualizarProductos.TabIndex = 4;
+            this.botonVisualizarProductos.Text = "PRODUCTO/S";
+            this.botonVisualizarProductos.UseVisualStyleBackColor = false;
+            // 
+            // botonVisualizarEstadisticas
+            // 
+            this.botonVisualizarEstadisticas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.botonVisualizarEstadisticas.FlatAppearance.BorderSize = 0;
+            this.botonVisualizarEstadisticas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonVisualizarEstadisticas.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonVisualizarEstadisticas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonVisualizarEstadisticas.Location = new System.Drawing.Point(599, 328);
+            this.botonVisualizarEstadisticas.Name = "botonVisualizarEstadisticas";
+            this.botonVisualizarEstadisticas.Size = new System.Drawing.Size(236, 53);
+            this.botonVisualizarEstadisticas.TabIndex = 5;
+            this.botonVisualizarEstadisticas.Text = "ESTADISTICAS";
+            this.botonVisualizarEstadisticas.UseVisualStyleBackColor = false;
             // 
             // MainWindowsPCG
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1008, 561);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.botonVisualizarEstadisticas);
+            this.Controls.Add(this.botonVisualizarProductos);
+            this.Controls.Add(this.botonVisualizarClientes);
+            this.Controls.Add(this.botonAgregarServicioVendido);
+            this.Controls.Add(this.botonAgregarProducto);
+            this.Controls.Add(this.botonAgregarCliente);
             this.Name = "MainWindowsPCG";
-            this.Text = "Project CG";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button botonAgregarCliente;
+        private System.Windows.Forms.Button botonAgregarProducto;
+        private System.Windows.Forms.Button botonAgregarServicioVendido;
+        private System.Windows.Forms.Button botonVisualizarClientes;
+        private System.Windows.Forms.Button botonVisualizarProductos;
+        private System.Windows.Forms.Button botonVisualizarEstadisticas;
     }
 }
 
